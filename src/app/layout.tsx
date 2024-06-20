f245b25bcdc6e1ce5296d8cd7e31fed75dce0e8c
Techanzy Limtied <techanzy@implementai.io>
2024-06-20 20:02:33 +0500

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,6 +69,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
       <body className={`${inter.className} bg-gray-50`}>{children}</body>
     </html>
   );
