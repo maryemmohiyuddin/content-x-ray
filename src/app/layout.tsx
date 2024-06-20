5ac555dca0f0a1c5803475f7ddcf2283d422acb9
Techanzy Limtied <techanzy@implementai.io>
2024-06-20 19:29:14 +0500

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Content X Ray",
+  description: "Instantly analyze your documents for deeper insights.",
+  openGraph: {
+    images: [
+      {
+        url: "/opengraph.png",
+        width: 1200,
+        height: 800,
+        alt: "Open Graph Image",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    images: "/twitter.png",
+  },
 };
 
 export default function RootLayout({
@@ -16,8 +31,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
-      <body className={ `${inter.className} bg-gray-50`}>{children}</body>
+      <head>
+        <meta property="og:title" content="Content X Ray" />
+        <meta
+          property="og:description"
+          content="Instantly analyze your documents for deeper insights."
+        />
+        <meta property="og:image" content="/opengraph.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Content X Ray" />
+        <meta
+          name="twitter:description"
+          content="Instantly analyze your documents for deeper insights."
+        />
+        <meta name="twitter:image" content="/twitter.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
+
+      <body className={`${inter.className} bg-gray-50`}>{children}</body>
     </html>
   );
 }
