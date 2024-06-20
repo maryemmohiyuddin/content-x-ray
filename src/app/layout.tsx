6df9c16f8c8af63a7400b945b04b6a163c8abc5c
Techanzy Limtied <techanzy@implementai.io>
2024-06-20 19:33:12 +0500

    og images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,14 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    images: "/twitter.png",
+    images: [
+      {
+        url: "/twitter.png",
+        width: 1200,
+        height: 630,
+        alt: "Twitter Image",
+      },
+    ],
   },
 };
 
@@ -32,18 +39,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta property="og:title" content="Content X Ray" />
-        <meta
-          property="og:description"
-          content="Instantly analyze your documents for deeper insights."
-        />
+       
         <meta property="og:image" content="/opengraph.png" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Content X Ray" />
-        <meta
-          name="twitter:description"
-          content="Instantly analyze your documents for deeper insights."
-        />
+       
         <meta name="twitter:image" content="/twitter.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
       </head>
